cab3294575092c597eddd8b9c0daa9ee47a2499b
Raj Shivaram <raj.shivaram@hotmail.com>
2023-05-24 22:44:04 +0530

    Initial commit

---
+++ src/main/java/com/example/hspspringappsample/HspSpringappSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hspspringappsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspSpringappSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspSpringappSampleApplication.class, args);
+	}
+
+}
