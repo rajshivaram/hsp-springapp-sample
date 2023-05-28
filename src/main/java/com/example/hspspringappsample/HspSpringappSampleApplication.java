ad151acc17e8af0bec017c10727a2adb0435cbdc
Raj Shivaram <raj.shivaram@hotmail.com>
2023-05-28 10:28:30 +0530

    .

--- src/main/java/com/example/hspspringappsample/HspSpringappSampleApplication.java
+++ src/main/java/com/example/hspspringappsample/HspSpringappSampleApplication.java
@@ -2,8 +2,12 @@ package com.example.hspspringappsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan
 public class HspSpringappSampleApplication {
 
 	public static void main(String[] args) {
