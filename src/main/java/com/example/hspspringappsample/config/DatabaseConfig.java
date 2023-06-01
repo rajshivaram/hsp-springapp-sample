f78194c122049e3101abb21f9141a5d6985e2a7b
Raj Shivaram <raj.shivaram@hotmail.com>
2023-06-01 20:19:01 +0530

    Update DatabaseConfig.java

--- src/main/java/com/example/hspspringappsample/config/DatabaseConfig.java
+++ src/main/java/com/example/hspspringappsample/config/DatabaseConfig.java
@@ -1,8 +1,10 @@
 package com.example.hspspringappsample.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -26,5 +28,19 @@ public class DatabaseConfig {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
+    }
+
+    @PostConstruct
+    public void initializeDatabase() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
+        String tableName = "users";
+
+        String createTableQuery = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+                + "id SERIAL PRIMARY KEY,"
+                + "name VARCHAR(255) NOT NULL,"
+                + "email VARCHAR(255) NOT NULL"
+                + ")";
+
+        jdbcTemplate.execute(createTableQuery);
     }
 }
